0210351d70a94258807b79d73e78220072d8bc30
Tom Ballingall <tom.ballingall@gmail.com>
2016-12-26 22:58:15 -0500

    Placeholder app. Getting routing error for static#home

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:create, :destroy]
 
+  root 'static#home'
   get 'login' => 'sessions#new', as: 'log_in'
   get 'logout' => 'sessions#destroy', as: 'log_out'
-
-  root 'static#home'
 end
